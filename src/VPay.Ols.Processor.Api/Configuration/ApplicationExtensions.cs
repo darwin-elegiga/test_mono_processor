f533568165863d0e209fac508475b638849ad7b0
Drew Pawlik <dpawlik@vpayusa.com>
2022-01-20 09:48:52 -0600

    Cleans up the project setup a bit

--- src/VPay.Ols.Processor.Api/Configuration/ApplicationExtensions.cs
+++ src/VPay.Ols.Processor.Api/Configuration/ApplicationExtensions.cs
@@ -1,26 +1,18 @@
 ﻿using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
-using Swashbuckle.AspNetCore.SwaggerUI;
 using VPay.AspNetCore.HealthChecks;
 
-namespace VPay.Ols.Processor.Api;
+namespace VPay.Ols.Processor.Api.Configuration;
 
 public static class ApplicationExtensions
 {
     public static void ConfigureMiddleware(this IApplicationBuilder app)
     {
         app
+            .UseHttpLogging()
             .UseProblemDetails()
-            .UseSwagger()
-            .UseSwaggerUI(c =>
-            {
-                const string Title = "OLS Processor API";
-                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", $"{Title} - v1.0");
-                c.EnableDeepLinking();
-                c.SetupVPayDocumentStyles(Title);
-            })
-            .UseRouting();
+            .ApplySwaggerMiddleware();
     }
 
     public static void ConfigureEndpoints(this IEndpointRouteBuilder app)
