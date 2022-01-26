f767b983dbc15f9cf13f430d9311c51bf664b604
John Ackerman <john@backslashdev.com>
2022-01-26 09:55:19 -0700

    Package updates, code review feedback

--- src/database/VPay.Ols.Processor.Database.SSDT/dbo/Stored Procedures/usp_OlsFile_Insert.sql
+++ src/database/VPay.Ols.Processor.Database.SSDT/dbo/Stored Procedures/usp_OlsFile_Insert.sql
@@ -1,25 +1,25 @@
 ﻿CREATE PROCEDURE [dbo].[usp_OlsFile_Insert]
 	@FileName NVARCHAR(255)
-    ,@FileHash CHAR(64)
-    ,@FileType NVARCHAR(20)
-    ,@Warnings NVARCHAR(MAX) = NULL
+	,@FileHash CHAR(64)
+	,@FileType NVARCHAR(20)
+	,@Warnings NVARCHAR(MAX) = NULL
 AS
 BEGIN
-    SET XACT_ABORT ON;
-    SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+	SET NOCOUNT ON;
 
-    INSERT INTO OlsFile (
-        DateProcessed,
-        [FileName],
-        FileHash,
-        FileType,
-        Warnings
-    )
-    VALUES (
-        GETDATE()
-        ,@FileName
-        ,@FileHash
-        ,@FileType
-        ,@Warnings
-    )
+	INSERT INTO OlsFile (
+		DateProcessed
+		,[FileName]
+		,FileHash
+		,FileType
+		,Warnings
+	)
+	VALUES (
+		GETDATE()
+		,@FileName
+		,@FileHash
+		,@FileType
+		,@Warnings
+	)
 END
