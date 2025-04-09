0ec7035cec207df6d42a911712f72f0c02d2f66a
Sravan Kumar Boyalakuntla <boyalakuntla_kumar+uhg@optum.com>
2025-04-09 20:52:56 +0530

    Added Code for adding quorum queue

---
+++ src/VPay.Ols.Processor.QueueConsumers/Consumers/AuthorizationFileConsumerDefinition.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit.ConsumeConfigurators;
+using MassTransit.Definition;
+using MassTransit.RabbitMqTransport;
+using MassTransit;
+
+namespace VPay.Ols.Processor.QueueConsumers.Consumers;
+
+public class AuthorizationFileConsumerDefinition : ConsumerDefinition<AuthorizationFileConsumer>
+{
+    protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<AuthorizationFileConsumer> consumerConfigurator)
+    {
+        if (endpointConfigurator is IRabbitMqReceiveEndpointConfigurator rabbit)
+        {
+            rabbit.SetQuorumQueue(3);
+        }
+
+        base.ConfigureConsumer(endpointConfigurator, consumerConfigurator);
+    }
+}
