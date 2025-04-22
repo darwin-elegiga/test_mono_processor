3ae960d56ac07b6860accd214db17c72b0c72043
Sravan Kumar Boyalakuntla <boyalakuntla_kumar@optum.com>
2025-04-22 20:48:13 +0530

    Pay-286566 -- Added Quorum queue related Changes  (#10)
    * Updated Vpay MassTransit Nuget package for qourum queue changes
    
    * Added Code for adding quorum queue
    
    * Added quorum  queue related code changes
    
    * Added Quorum Queue related changes
    
    * Updated Vpay MassTransit Nuget package and code as per latest changes

---
+++ src/VPay.Ols.Processor.QueueConsumers/Consumers/PostedTransactionFileProcessedConsumerDefinition.cs
@@ -0,0 +1,11 @@
+﻿using MassTransit.ConsumeConfigurators;
+using MassTransit.Definition;
+using MassTransit.RabbitMqTransport;
+using MassTransit;
+using VPay.MassTransit.RabbitMqTransport;
+
+namespace VPay.Ols.Processor.QueueConsumers.Consumers;
+
+public class PostedTransactionFileProcessedConsumerDefinition : QuorumConsumerDefinition<PostedTransactionFileProcessedConsumer>
+{
+}
